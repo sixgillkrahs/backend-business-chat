956b976e2d6515f93146968c76c6be3ca8f5c0c2
quandv <quan.do@loxtek.vn>
2026-07-13 11:13:05 +0700

    format repsonse

--- internal/interface/http/handlers/auth_handler.go
+++ internal/interface/http/handlers/auth_handler.go
@@ -5,6 +5,7 @@ import (
 
 	"github.com/gin-gonic/gin"
 	"github.com/sixgillkrahs/backend-business-chat/internal/application"
+	"github.com/sixgillkrahs/backend-business-chat/pkg/utils"
 )
 
 type AuthHandler struct {
@@ -18,8 +19,8 @@ func NewAuthHandler(authService *application.AuthService) AuthHandler {
 func (h *AuthHandler) ListActions(c *gin.Context) {
 	actions, err := h.AuthService.GetAllActions(c.Request.Context())
 	if err != nil {
-		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+		utils.Error(c, http.StatusInternalServerError, err.Error())
 		return
 	}
-	c.JSON(http.StatusOK, actions)
+	utils.Success(c, actions)
 }
